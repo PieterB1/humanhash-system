843759b42c31859204fa755a102df29cd10249da
PieterB1 <pieterwjbouwer@icloud.com>
2025-05-20 11:33:55 +0200

    Update .gitignore to ignore node_modules and artifacts, commit package updates and index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BiometricUpload from './components/BiometricUpload.tsx';
+import BiometricUpload from './components/BiometricUpload';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
